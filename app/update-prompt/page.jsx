f6a5abb48ce0789a5119f8555bd55c0dc62671f1
Oliver Štroch <oliverstroch@Oliver-MacBook-Air.local>
2024-04-29 22:09:00 +0200

    Updated files

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,36 +1,30 @@
 "use client";
 
-import { useEffect, useState, Suspense } from "react";
-import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
 
-import Form from "@components/Form"; // Import the Form component
+import Form from "@components/Form";
 
 const UpdatePrompt = () => {
   const router = useRouter();
-  const promptId = router.query?.id || ""; // Check if router.query is defined before accessing id
+  const searchParams = useSearchParams();
+  const promptId = searchParams.get("id");
 
-  const [post, setPost] = useState({ prompt: "", tag: "" });
+  const [post, setPost] = useState({ prompt: "", tag: "", });
   const [submitting, setIsSubmitting] = useState(false);
 
   useEffect(() => {
     const getPromptDetails = async () => {
-      try {
-        if (promptId) {
-          const response = await fetch(`/api/prompt/${promptId}`);
-          const data = await response.json();
+      const response = await fetch(`/api/prompt/${promptId}`);
+      const data = await response.json();
 
-          setPost({
-            prompt: data.prompt,
-            tag: data.tag,
-          });
-        }
-      } catch (error) {
-        console.log(error);
-      }
+      setPost({
+        prompt: data.prompt,
+        tag: data.tag,
+      });
     };
 
-    // Fetch prompt details only if promptId exists
-    getPromptDetails();
+    if (promptId) getPromptDetails();
   }, [promptId]);
 
   const updatePrompt = async (e) => {
@@ -59,17 +53,13 @@ const UpdatePrompt = () => {
   };
 
   return (
-    <div>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Form // Use the imported Form component
-          type="Edit"
-          post={post}
-          setPost={setPost}
-          submitting={submitting}
-          handleSubmit={updatePrompt}
-        />
-      </Suspense>
-    </div>
+    <Form
+      type='Edit'
+      post={post}
+      setPost={setPost}
+      submitting={submitting}
+      handleSubmit={updatePrompt}
+    />
   );
 };
 
